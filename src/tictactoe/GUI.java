16a8638e91c5a3d4df27773cdfbc3e9897da5772
IIpu3pak <IIpu3pak7@gmail.com>
2016-01-11 22:51:17 +0200

    change path to fxml files

--- src/tictactoe/GUI.java
+++ src/tictactoe/GUI.java
@@ -15,9 +15,7 @@ public class GUI extends Application {
     public void start(Stage primaryStage) throws Exception{
 
         try {
-            System.out.println("brrrrr");
             Parent root = FXMLLoader.load(getClass().getResource("/fxml/application.fxml"));
-            System.out.println("brrrrr");
             primaryStage.setTitle("Tic tac toe");
 
             Scene scene = new Scene(root, 600, 625);
